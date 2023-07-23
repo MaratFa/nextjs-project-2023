a392671712ede713ccd70bb0fc19ddce2a4cfa35
Marat <evolution99@rambler.ru>
2023-07-23 18:06:21 +0300

    Project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section className="flex-start flex-col padding mb-16">
+            Categories
+            Posts
+            LoadMore
+        </section>
+    )
+}
+
+export default Home;
