07ad97938af4521c33508f7ea1956c4a8ccf1b4b
Marat <evolution99@rambler.ru>
2023-07-25 23:35:55 +0300

    Edited Footer.tsx in components folder

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,17 @@
 import { footerLinks } from '@/constants'
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link';
 
-const FooterColumn = () => (
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+}
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className="footer_column">
-    <h4>Title</h4>
+    <h4 className="font-semibold">{title}</h4>
     <ul className="flex flex-col gap-2 font-normal">
-      Links
+      {links.map((link) => <Link href="/" key={link}>{link}</Link>)}
     </ul>
   </div>
 )
@@ -23,12 +28,24 @@ const Footer = () => {
             alt="Flexible"
           />
 
-          <p className="text-start text-sm fount-normal mt-5 max-w-xs">
-            Flexibble is the world's leading community for creatives to share, grow, and get hired.
-          </p>
+          <p className="text-start text-sm fount-normal mt-5 max-w-xs">Flexibble is the world's leading community for creatives to share, grow, and get hired.</p>
         </div>
         <div className="flex flex-wrap gap-12">
-          <FooterColumn title={footerLinks} links={footerLinks[0].links} />
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links} />
+            <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links} />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links} />
+            <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links} />
         </div>
       </div>
     </footer>
