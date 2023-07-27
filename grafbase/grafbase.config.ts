838d6b2533c5ec40cac78206ac0798bba051045c
Marat <evolution99@rambler.ru>
2023-07-27 21:41:41 +0300

    Edited grafbase.config.ts to grafbase folder

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -7,14 +7,17 @@ const User = g.model('User', {
   description: g.string().optional(),
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
-  projects: g.relation(),
+  projects: g.relation(() => Project).list().optional(),
 })
 
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(),
-
-  
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
 })
 
 export default config({
