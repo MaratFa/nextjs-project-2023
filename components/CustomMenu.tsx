950d123f7662dfd952043185f0d6b65535a1e93c
Marat <evolution99@rambler.ru>
2023-08-02 23:49:16 +0300

    Edited CustomMenu.tsx in components folder

--- components/CustomMenu.tsx
+++ components/CustomMenu.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { Menu } from '@headlessui/react';
 import Image from 'next/image'
 
@@ -18,7 +17,7 @@ const CustomMenu = ({ title, state, filters, setState }: Props) => {
             <Menu as="div" className="self-start relative">
                 <div>
                     <Menu.Button className="flexCenter custom_menu-btn">
-                        {state || 'Select a category'}
+                        {state || 'Category'}
                         <Image
                             src="/arrow-down.svg"
                             width={10}
@@ -28,9 +27,18 @@ const CustomMenu = ({ title, state, filters, setState }: Props) => {
                     </Menu.Button>
                 </div>
                 <Menu.Items className="flexStart custom_menu-items">
-
-
-
+                    {filters.map((tag) => (
+                        <Menu.Item key={tag}>
+                            <button
+                                type="button"
+                                value={tag}
+                                className="custom_menu-item"
+                                onClick={(e) => setState(e.currentTarget.value)}
+                            >
+                                {tag}
+                            </button>
+                        </Menu.Item>
+                    ))}
                 </Menu.Items>
             </Menu>
         </div>
