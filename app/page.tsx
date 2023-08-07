d588359578bc6d2f8cb181fbb16d9408d3eb4eb5
Marat <evolution99@rambler.ru>
2023-08-08 02:26:27 +0300

    Edited page.tsx in app folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import { ProjectInterface } from "@/common.types";
 import { fetchAllProjects } from "@/lib/actions";
 
+type SearchParams = {
+    category?: string | null;
+    endcursor?: string | null;
+}
+
+type Props = {
+    searchParams: SearchParams
+}
+
 type ProjectSearch = {
     projectSearch: {
         edges: { node: ProjectInterface }[];
@@ -13,9 +22,20 @@ type ProjectSearch = {
     },
 }
 
-const Home = async () => {
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+    const data = await fetchAllProjects(category, endcursor) as ProjectSearch;
 
+    const projectsToDisplay = data?.projectSearch?.edges || [];
 
+    if (projectsToDisplay.length === 0) {
+        return (
+            <section className="flexStart flex-col paddings">
+                Categories
+
+                <p className="no-result-text text-center">No projects found, go create some first.</p>
+            </section>
+        )
+    }
 
     return (
         <section className="flex-start flex-col paddings mb-16">
