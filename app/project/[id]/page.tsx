dab962f2df64f26fa29c8df4e8fe15448b0d080b
Marat <evolution99@rambler.ru>
2023-08-10 08:02:34 +0300

    Added page.tsx to [id] folder

---
+++ app/project/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { getCurrentUser } from '@/lib/session'
+import React from 'react'
+
+const Project = async () => {
+    const session = await getCurrentUser()
+
+    return (
+        <div>page</div>
+    )
+}
+
+export default Project
