cc81840c1a7a1c34bf0d9508df63e577b38248b6
Marat <evolution99@rambler.ru>
2023-08-11 07:44:53 +0300

    Edited page.tsx in [id] folder

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,8 +1,16 @@
+import { ProjectInterface } from '@/common.types';
+import { getProjectDetails } from '@/lib/actions';
 import { getCurrentUser } from '@/lib/session'
-import React from 'react'
 
-const Project = async () => {
-    const session = await getCurrentUser()
+const Project = async ({ params: { id } }: { params: { id: string } }) => {
+    const session = await getCurrentUser();
+    const result = await getProjectDetails(id) as { project?: ProjectInterface };
+
+    if (!result?.project) {
+        <p>Failed to fetch project information</p>
+    }
+
+    console.log(result?.project)
 
     return (
         <div>page</div>
