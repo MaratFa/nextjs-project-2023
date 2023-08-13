fd12617597d7c2b5d9b6889ea59be38c57fd2d77
Marat <evolution99@rambler.ru>
2023-08-13 22:01:39 +0300

    Edited RelatedProjects.tsx in components folder

--- components/RelatedProjects.tsx
+++ components/RelatedProjects.tsx
@@ -1,6 +1,7 @@
 import { UserProfile, ProjectInterface } from "@/common.types";
 import { getUserProjects } from "@/lib/actions";
 import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
     userId: string;
@@ -11,6 +12,8 @@ const RelatedProjects = async ({ userId, projectId }: Props) => {
     const result = await getUserProjects(userId) as { user?: UserProfile }
 
     const filteredProjects = result?.user?.projects?.edges?.filter(({ node }: { node: ProjectInterface }) => node?.id != projectId)
+
+    console.log(filteredProjects)
 
     if (filteredProjects?.length === 0) return null;
 
@@ -25,7 +28,21 @@ const RelatedProjects = async ({ userId, projectId }: Props) => {
                     View All
                 </Link>
             </div>
-        </section>
+
+            <div className="related_projects-grid">
+                {filteredProjects?.map(({ node }: { node: ProjectInterface }) => (
+                    <div className="flexCenter related_project-card drop-shadow-card">
+                        <Link href={`/project/${node?.id}`} className="flexCenter group relative w-full h-full" >
+                            <Image src={node?.image} width={414} height={314} className="w-full h-full object-cover rounded-2xl" alt="project image" />
+
+                            <div className="hidden group-hover:flex related_project-card_title">
+                                <p className="w-full">{node?.title}</p>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </section >
     )
 }
 
